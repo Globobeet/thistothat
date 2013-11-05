d2312f3d0848aef4a48bb0230a918620d071dbf3
Josh Lasdin <joshlasdindesign@gmail.com>
2013-11-04 20:07:08 -0500

    Install screen js & css

---
+++ config.rb
@@ -0,0 +1,25 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "lib/min"
+sass_dir = "lib/sass"
+images_dir = "lib/img"
+javascripts_dir = "lib/js"
+fonts_dir = "lib/font"
+
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass lib/sass scss && rm -rf sass && mv scss sass
+preferred_syntax = :scss
