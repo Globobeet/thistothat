89bab524213faa8bb7ad7aa157daa004fbbd1bfc
Josh Lasdin <joshlasdindesign@gmail.com>
2013-12-18 15:10:01 -0500

    Prepped for Jasmine JS testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,14 +68,32 @@ module.exports = function(grunt) {
             }
         },
 
+        jasmine: {
+            src : modelPath + '/**/*.js',
+            options: {
+                specs : 'tests/*_test.js',
+                vendor: [
+                    minPath + '/libraries.min.js',
+                    'lib/conversions.js'
+                ],
+                helpers : [
+                    jsPath + '/Helpers.js'
+                ]
+            }
+        },
+
         watch: {
             libraries: {
                 files: ['bower_components/**/*'],
                 tasks: ['compile-libs']
             },
             scripts: {
-                files: [modelPath + '/*.js', jsPath + '/*.js'],
-                tasks: ['compile-js']
+                files: [jsPath + '/**/*.js'],
+                tasks: ['test-js', 'compile-js']
+            },
+            tests: {
+                files: 'tests/**/*_test.js',
+                tasks: ['test-js']
             },
             css: {
                 files: ['lib/sass/*.scss'],
@@ -90,11 +108,13 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-bower-concat');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
 
     grunt.registerTask('compile-css', ['compass']);
     grunt.registerTask('compile-libs', ['bower_concat']);
     grunt.registerTask('compile-js', ['jshint:source', 'concat', 'jshint:afterconcat']);
     grunt.registerTask('compile', ['compile-css', 'compile-libs', 'compile-js']);
-    grunt.registerTask('compile-prod', ['compile', 'uglify']);
+    grunt.registerTask('test-js', ['jasmine']);
+    grunt.registerTask('compile-prod', ['test-js', 'compile', 'uglify']);
     grunt.registerTask('default', ['compile', 'watch']);
 };
